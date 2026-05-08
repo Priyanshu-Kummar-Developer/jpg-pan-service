f7f69f1f0f2be11cc5197fd41bdb95e092049ddb
Priyanshu Kumar <priyanshujaiswara@gmail.com>
2026-05-09 02:24:11 +0530

    Version3

--- src/data/services.ts
+++ src/data/services.ts
@@ -1,263 +1,312 @@
 const services = [
 
-  {
-    title: "New RC PDF",
-    charge: "₹20/",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
-  },
+    {
+        title: "New Pan Card Apply",
+        charge: "₹250",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "Find Lost Pan",
-    charge: "₹100",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
-  },
+    {
+        title: "Correction Pan Card Apply",
+        charge: "₹250",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "Voter Card Advance PDF",
-    charge: "₹20",
-    category: "Voter",
-    logo: "https://cdn-icons-png.flaticon.com/512/681/681494.png",
-  },
+    {
+        title: "Minor Pan Card Apply",
+        charge: "₹200",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "UID To Mobile Owner Details",
-    charge: "₹50",
-    category: "Aadhaar",
-    logo: "https://cdn-icons-png.flaticon.com/512/3059/3059518.png",
-  },
+    {
+        title: "Minor to Major Pan Card Apply",
+        charge: "₹200",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "Mobile Number Owner Details",
-    charge: "₹30",
-    category: "Mobile",
-    logo: "https://cdn-icons-png.flaticon.com/512/724/724664.png",
-  },
+    {
+        title: "Pan Card Correction - Full Name/Father's Name Change",
+        charge: "₹2000",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "UP Edistrict PDF Instant",
-    charge: "₹10/PDF",
-    category: "Edistrict",
-    logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
-  },
+    {
+        title: "Vehicle RC Download",
+        charge: "₹120/-",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
+    },
 
-  {
-    title: "Masked Pan",
-    charge: "₹1/Search",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942748.png",
-  },
+    {
+        title: "Find Lost Pan",
+        charge: "₹300",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "Pan Name Verify",
-    charge: "₹2/-",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
-  },
+    {
+        title: "Voter Card PDF",
+        charge: "₹400",
+        category: "Voter",
+        logo: "https://cdn-icons-png.flaticon.com/512/681/681494.png",
+    },
 
-  {
-    title: "UP Kisan Card",
-    charge: "₹20/PDF",
-    category: "Kisan",
-    logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
-  },
+    {
+        title: "Online Aadhar Address Change(Without Documents)",
+        charge: "₹600",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3059/3059518.png",
+    },
 
-  {
-    title: "All State Kisan Card",
-    charge: "₹20/PDF",
-    category: "Kisan",
-    logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
-  },
+    {
+        title: "UID To Mobile Owner Details",
+        charge: "₹300",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3059/3059518.png",
+    },
 
-  {
-    title: "Pan Card Advance Print",
-    charge: "₹20",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
-  },
+    {
+        title: "Mobile Number Owner Details",
+        charge: "₹500",
+        category: "Mobile",
+        logo: "https://cdn-icons-png.flaticon.com/512/724/724664.png",
+    },
 
-  {
-    title: "UP Edistrict Advance",
-    charge: "₹10/PDF",
-    category: "Edistrict",
-    logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
-  },
+    {
+        title: "UP Edistrict PDF",
+        charge: "₹50/PDF",
+        category: "Edistrict",
+        logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+    },
 
-  {
-    title: "UP Family ID Card",
-    charge: "₹5/PDF",
-    category: "Family ID",
-    logo: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
-  },
+    {
+        title: "Masked Pan",
+        charge: "₹30/Search",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942748.png",
+    },
 
-  {
-    title: "IGNOU Result",
-    charge: "₹0.50/PDF",
-    category: "Education",
-    logo: "https://cdn-icons-png.flaticon.com/512/3976/3976626.png",
-  },
+    {
+        title: "Pan Name Verify",
+        charge: "₹30/-",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
+    },
 
-  {
-    title: "Ration Card PDF 2.0",
-    charge: "₹20",
-    category: "Ration",
-    logo: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
-  },
+    {
+        title: "UP Kisan Card",
+        charge: "₹100/PDF",
+        category: "Kisan",
+        logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
+    },
 
-  {
-    title: "OLD RC PDF",
-    charge: "₹20/",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
-  },
+    {
+        title: "All State Kisan Card",
+        charge: "₹100/PDF",
+        category: "Kisan",
+        logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
+    },
 
-  {
-    title: "UP AgriStack Status",
-    charge: "Free",
-    category: "Kisan",
-    logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
-  },
+    {
+        title: "Pan Card Print",
+        charge: "₹120",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "DL HD PVC",
-    charge: "₹20/PDF",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
-  },
+    {
+        title: "UP Edistrict Advance",
+        charge: "₹100/PDF",
+        category: "Edistrict",
+        logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+    },
 
-  {
-    title: "Aadhaar Manual",
-    charge: "₹20/PDF",
-    category: "Aadhaar",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-  },
+    {
+        title: "UP Family ID Card",
+        charge: "₹100/PDF",
+        category: "Family ID",
+        logo: "https://cdn-icons-png.flaticon.com/512/747/747376.png",
+    },
 
-  {
-    title: "DL PDF Download",
-    charge: "₹20/PDF",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
-  },
+    {
+        title: "IGNOU Result",
+        charge: "₹100/PDF",
+        category: "Education",
+        logo: "https://cdn-icons-png.flaticon.com/512/3976/3976626.png",
+    },
 
-  {
-    title: "RC PDF Server II",
-    charge: "₹20/",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
-  },
+    {
+        title: "Ration Card PDF 2.0",
+        charge: "₹100/PDF",
+        category: "Ration",
+        logo: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
+    },
 
-  {
-    title: "DL Original PDF",
-    charge: "₹30",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
-  },
+    {
+        title: "OLD RC PDF",
+        charge: "₹120/PDF",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
+    },
 
-  {
-    title: "Download All UP Edistrict Docs",
-    charge: "₹20",
-    category: "Edistrict",
-    logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
-  },
+    {
+        title: "UP AgriStack Status",
+        charge: "10/-",
+        category: "Kisan",
+        logo: "https://cdn-icons-png.flaticon.com/512/2909/2909767.png",
+    },
 
-  {
-    title: "UP Old Birth To New Birth",
-    charge: "₹120",
-    category: "Birth Certificate",
-    logo: "https://cdn-icons-png.flaticon.com/512/2920/2920277.png",
-  },
+    {
+        title: "DL HD PVC",
+        charge: "₹120/PDF",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
+    },
 
-  {
-    title: "Aadhaar Status Check",
-    charge: "Free",
-    category: "Aadhaar",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-  },
+    {
+        title: "Aadhaar Manual",
+        charge: "₹400/PDF",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    },
 
-  {
-    title: "Pan To UID Find",
-    charge: "₹150",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
-  },
+    {
+        title: "DL PDF Download",
+        charge: "₹120/PDF",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
+    },
 
-  {
-    title: "Voter Card Manual",
-    charge: "₹20",
-    category: "Voter",
-    logo: "https://cdn-icons-png.flaticon.com/512/681/681494.png",
-  },
+    {
+        title: "RC PDF Server II",
+        charge: "₹120/",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
+    },
 
-  {
-    title: "Pan Card Manual Print",
-    charge: "₹15",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
-  },
+    {
+        title: "DL Original PDF",
+        charge: "₹120",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
+    },
 
-  {
-    title: "Validate Pan Card",
-    charge: "Free",
-    category: "PAN Card",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
-  },
+    {
+        title: "Download All UP Edistrict Docs",
+        charge: "₹100",
+        category: "Edistrict",
+        logo: "https://cdn-icons-png.flaticon.com/512/2991/2991148.png",
+    },
 
-  {
-    title: "Find Aadhaar by UDID",
-    charge: "₹30",
-    category: "Aadhaar",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-  },
+    {
+        title: "UP Old Birth To New Birth",
+        charge: "₹750",
+        category: "Birth Certificate",
+        logo: "https://cdn-icons-png.flaticon.com/512/2920/2920277.png",
+    },
 
-  {
-    title: "Vehicle PUC Apply",
-    charge: "₹60-100",
-    category: "Vehicle",
-    logo: "https://cdn-icons-png.flaticon.com/512/854/854894.png",
-  },
+    {
+        title: "Aadhaar Status Check",
+        charge: "10/-",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    },
 
-  {
-    title: "Check Vehicles Challan",
-    charge: "₹2",
-    category: "Vehicle",
-    logo: "https://cdn-icons-png.flaticon.com/512/595/595067.png",
-  },
+    {
+        title: "Pan To Aadhar Find",
+        charge: "₹450",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "DL Verification",
-    charge: "Free",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
-  },
+    {
+        title: "Voter Card Manual",
+        charge: "₹400",
+        category: "Voter",
+        logo: "https://cdn-icons-png.flaticon.com/512/681/681494.png",
+    },
 
-  {
-    title: "Find Ration By UID",
-    charge: "₹10",
-    category: "Ration",
-    logo: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
-  },
+    {
+        title: "Pan Card Manual Print",
+        charge: "₹120",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/1041/1041916.png",
+    },
 
-  {
-    title: "Learning Licence",
-    charge: "₹150",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
-  },
+    {
+        title: "Validate Pan Card",
+        charge: "10/-",
+        category: "PAN Card",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135768.png",
+    },
 
-  {
-    title: "RC Owner Mobile Find",
-    charge: "₹20",
-    category: "RC & DL",
-    logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
-  },
+    {
+        title: "Find Aadhaar by UDID",
+        charge: "₹300",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    },
 
-  {
-    title: "Aadhaar Advance",
-    charge: "₹20/PDF",
-    category: "Aadhaar",
-    logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
-  },
+    {
+        title: "Vehicle PUC - Two Wheeler",
+        charge: "₹160",
+        category: "Vehicle",
+        logo: "https://cdn-icons-png.flaticon.com/512/854/854894.png",
+    },
+
+    {
+        title: "Vehicle PUC - Four Wheeler",
+        charge: "₹200",
+        category: "Vehicle",
+        logo: "https://cdn-icons-png.flaticon.com/512/854/854894.png",
+    },
+
+    {
+        title: "Check Vehicles Challan",
+        charge: "₹20",
+        category: "Vehicle",
+        logo: "https://cdn-icons-png.flaticon.com/512/595/595067.png",
+    },
+
+    {
+        title: "DL Verification",
+        charge: "10/-",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
+    },
+
+    {
+        title: "Find Ration By UID",
+        charge: "₹120",
+        category: "Ration",
+        logo: "https://cdn-icons-png.flaticon.com/512/3075/3075977.png",
+    },
+
+    {
+        title: "Learning Licence",
+        charge: "₹450",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/942/942799.png",
+    },
+
+    {
+        title: "RC Owner Mobile Find",
+        charge: "₹120",
+        category: "RC & DL",
+        logo: "https://cdn-icons-png.flaticon.com/512/2554/2554936.png",
+    },
+
+    {
+        title: "Aadhaar Manual Print",
+        charge: "₹450/PDF",
+        category: "Aadhaar",
+        logo: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+    },
 
 ];
 
