8a22652ed294606bc4096179334d6a5feddfdecd
Priyanshu Kumar <priyanshujaiswara@gmail.com>
2026-05-22 02:49:11 +0530

    notification add

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import OneSignal from 'react-onesignal'
+
+async function initOneSignal() {
+  await OneSignal.init({
+    appId: '34d731a7-3cc6-4245-9bff-788ab8c340d2',
+    allowLocalhostAsSecureOrigin: true,
+  });
+  OneSignal.Notifications.requestPermission();
+}
+initOneSignal();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
